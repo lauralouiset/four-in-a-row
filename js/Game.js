ee95aed2087a251bd0cf7c2f7723e57fb0a6aeee
Laura-Louise Tobin <lauralouisetobin@gmail.com>
2018-10-09 18:41:00 -0400

    Initialized all object

--- js/Game.js
+++ js/Game.js
@@ -1,5 +1,22 @@
 class Game{
 	constructor(){
-		
+		this.ready = false;
+		this.board = new Board();
+		this.players = this.createPlayers();
 	}
+
+/* 
+	* Creates two player objects for the game
+	* @return {Array}		An array of two Player objects
+*/
+
+	createPlayers(){
+		const players = [
+				new Player(`Player 1`, 1, `#e15258, true`),
+				new Player(`Player 2`, 2, `#359a13`)
+				];
+				
+		return players;
+	}
+
 }
